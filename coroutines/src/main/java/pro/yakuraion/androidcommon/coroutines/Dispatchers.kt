06965ebc68fbb93a6b7b773657926d43c1cd2ff0
Vyacheslav Yakovlev <yakuraion@gmail.com>
2023-03-15 23:07:40 +0300

    remove Dispatchers from :coroutines

--- coroutines/src/main/java/pro/yakuraion/androidcommon/coroutines/Dispatchers.kt
+++
@@ -1,9 +0,0 @@
-package pro.yakuraion.androidcommon.coroutines
-
-import kotlinx.coroutines.CoroutineDispatcher
-
-class Dispatchers(
-    val ioDispatcher: CoroutineDispatcher,
-    val computeDispatcher: CoroutineDispatcher,
-    val mainDispatcher: CoroutineDispatcher
-)
