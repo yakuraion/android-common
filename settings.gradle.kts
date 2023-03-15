e8c3e3da488ba622dd01af26ab90f2318ceff1c2
Vyacheslav Yakovlev <yakuraion@gmail.com>
2023-03-15 21:28:45 +0300

    rename to "android-common"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,6 +14,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Common"
+rootProject.name = "android-common"
 
 include(":kotlin")
